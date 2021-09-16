124d354377ccfb5ef41becb4ab7d2df0ab9da6b5
Yaroslav <lixometr@gmail.com>
2021-09-16 10:56:04 +0300

    fix

---
+++ js/styles.js
@@ -0,0 +1,4 @@
+(()=>{"use strict";var __webpack_modules__={255:()=>{eval(`// extracted by mini-css-extract-plugin
+
+
+//# sourceURL=webpack://freelancehunt/./src/css/styles.css?`)}},__webpack_exports__={};__webpack_modules__[255]()})();
